80f011364e29edc2b401b3df73d1ef3d84456fec
Oscar Alonso <oscar.alonso@posibillian.tech>
2024-07-26 11:18:09 -0400

    Inicio de SprigBoot

---
+++ src/main/java/org/oiab/springbootcicd/SpringbootCiCdApplication.java
@@ -0,0 +1,13 @@
+package org.oiab.springbootcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/oiab/springbootcicd/SpringbootCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oiab.springbootcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
