b627eff49dcba72ef52251c7bc884a05e545854e
Oscar Alonso <oscar.alonso@posibillian.tech>
2024-07-26 11:22:22 -0400

    Agregamos endpoint para welcome

--- src/main/java/org/oiab/springbootcicd/SpringbootCiCdApplication.java
+++ src/main/java/org/oiab/springbootcicd/SpringbootCiCdApplication.java
@@ -2,9 +2,17 @@ package org.oiab.springbootcicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootCiCdApplication {
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome to ...";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootCiCdApplication.class, args);
